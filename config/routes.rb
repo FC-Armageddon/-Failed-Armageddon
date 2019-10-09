ab7e7748db8aafcadadba2687a8f1b6ca5a920d2
daiki0809 <cookietanaka0809@gmail.com>
2019-10-09 03:20:56 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :users
 
 resources :users, only: [:update, :show, :edit]
 post 'users/deleted/:id' => 'users#deleted_flag', as:'deleted_flag'
 get 'users/admins_index' => "users#admins_index", as:'users_admins'
 get 'users/admins/:id' => 'users#admins_show', as:'user_admins'
 post 'users/admins_edit/:id' => 'users#admins_edit', as:'user_admins_edit'
-post 'users/admins/:id' => 'users#admins_update', as:'user_admins_update'
+post 'users/admins/:id' => 'users#admins_update', as:'user_admins'
 post 'users/admins/deleted/:id' => 'users#admins_deleted_flag', as:'admins_deleted_flag'
 
 resources :cds, only: [:index, :search, :show]
@@ -16,11 +14,11 @@ get 'cds/admins_index' => 'cds#admins_index', as:'cds_admins'
 get 'cds/search', as: 'search'
 get 'cds/admins/:id' => 'cds#admins_show', as:'cd_admins'
 get 'cds/admins_edit/:id' => 'cds#admins_edit', as:'cd_adimns_edit'
-post 'cds/admins/:id' => 'cds#admins_update', as:'cd_admins_update'
-delete 'cds/admins/:id' => 'cds#admins_destroy', as:'cd_admins_destroy'
+post 'cds/admins/:id' => 'cds#admins_update', as:'cd_admins'
+delete 'cds/admins/:id' => 'cds#admins_destroy', as:'cd_admins'
 post 'cds/admins' => 'cds#admins_create', as:'cds_new_create'
 get 'cds/admins_new' => 'cds#admins_new', as:'cds_new'
-get 'cds/admins_search', as:'admins_search'
+get 'cds/admins_search', as: 'admins_search'
 
 resources :genres, only: [:create, :update]
 
@@ -34,12 +32,13 @@ resources :songs, only: [:create, :update]
 
 resources :arrivals, only: [:create, :index]
 
-resources :carts, only: [:index, :create, :destroy, :update]
+resources :carts, only: [:index, :create, :destroy :update]
 post 'carts/deleted/:id' => 'carts#deleted_flag', as:'carts_deleted_flag'
 
 resources :buy_informations, only: [:new, :create, :updates]
 
 
-
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
