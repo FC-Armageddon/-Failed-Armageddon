2e5578470504ea5c6e044b098f79db66b02f22c1
7painapo7 <spurs11714@gmail.com>
2019-10-09 08:30:00 +0000

    [Add]アソシエーション

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,4 +1,12 @@
 class Cd < ApplicationRecord
+	has_many :genres
+	has_many :labels
+	has_many :sales_statuses
+	has_many :artists
+
+	belongs_to :disc
+	belongs_to :arrival
+
 	validates :genre_id, presence: true
 	validates :label_id, presence: true
 	validates :sales_status_id, presence: true
